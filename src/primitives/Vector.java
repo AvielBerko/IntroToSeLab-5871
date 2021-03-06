bf6737c513a1ac522a16c5c835c02dd66f045673
Meir Klemfner <meirk251@outlook.com>
2021-03-06 23:59:02 +0200

    Impl of equals, hashCode and toString for Vector.

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,5 +1,7 @@
 package primitives;
 
+import java.util.Objects;
+
 public class Vector {
     final Point3D _head;
 
@@ -13,5 +15,23 @@ public class Vector {
 
     public Vector(double x, double y, double z) {
         _head = new Point3D(x, y, z);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector" + _head;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector vector = (Vector) o;
+        return Objects.equals(_head, vector._head);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_head);
     }
 }
