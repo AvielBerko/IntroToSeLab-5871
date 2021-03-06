b2102890d6242bd3bcaf58e19a2483755e9673d5
Aviel Berkowitz <9155988@gmail.com>
2021-03-07 00:03:44 +0200

    Vector actions added

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,7 +1,7 @@
 package primitives;
 
 public class Vector {
-    final Point3D _head;
+    Point3D _head;
 
     public Vector(Point3D head) {
         this(head._x, head._y, head._z);
@@ -14,4 +14,53 @@ public class Vector {
     public Vector(double x, double y, double z) {
         _head = new Point3D(x, y, z);
     }
+
+    public Vector add(Vector other) {
+        return new Vector(_head._x.coord + other._head._x.coord,
+                          _head._y.coord + other._head._y.coord,
+                          _head._z.coord + other._head._z.coord);
+    }
+
+    public Vector subtract(Vector other) {
+        return new Vector(_head._x.coord - other._head._x.coord,
+                _head._y.coord - other._head._y.coord,
+                _head._z.coord - other._head._z.coord);
+    }
+
+    public Vector scale (double scalar) {
+        return new Vector(_head._x.coord * scalar, _head._y.coord * scalar, _head._z.coord * scalar);
+    }
+
+    public Vector crossProduct(Vector other) {
+        return new Vector(_head._y.coord * other._head._z.coord - _head._z.coord * other._head._y.coord,
+                          _head._x.coord * other._head._z.coord - _head._z.coord * other._head._x.coord,
+                          _head._x.coord * other._head._y.coord - _head._y.coord * other._head._x.coord);
+    }
+
+    public double dotProduct(Vector other) {
+        return _head._x.coord * other._head._x.coord +
+                _head._y.coord * other._head._y.coord +
+                _head._z.coord * other._head._z.coord;
+    }
+
+    public double lengthSquared() {
+        return _head.distanceSquared(Point3D.ZERO);
+    }
+
+    public double length() {
+        return Math.sqrt(lengthSquared());
+    }
+
+    public Vector normalize() {
+        double len = 1 / length();
+        _head = new Point3D(_head._x.coord * len, _head._y.coord * len, _head._z.coord * len);
+        return this;
+    }
+
+    public Vector normalized() {
+        double len = 1 / length();
+        return new Vector(new Point3D(_head._x.coord * len, _head._y.coord * len, _head._z.coord * len);)
+    }
+
+
 }
