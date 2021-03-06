9d092fe864678084ea9488a04078b00f1b11edc5
Meir Klemfner <meirk251@outlook.com>
2021-03-07 00:43:42 +0200

    Started to implement the tests for Vector.

--- tests/primitives/VectorTest.java
+++ tests/primitives/VectorTest.java
@@ -6,6 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VectorTest {
 
+    Vector _a = new Vector(3d, -3d, 3d);
+    Vector _aSame = new Vector(3d, -3d, 3d);
+    Vector _b = new Vector(-2.75d, 7d, -0.3d);
+
     @Test
     void add() {
     }
@@ -36,9 +40,35 @@ class VectorTest {
 
     @Test
     void normalize() {
+        Vector unit = new Vector(-0.365d, -0.93d, 0.399d);
+        Vector b = new Vector(_b._head).normalize();
+        assertEquals(b, unit);
     }
 
     @Test
     void normalized() {
+        Vector unit = new Vector(0.802d, -0.535d, 0.267d);
+        assertEquals(_a.normalized(), unit);
     }
+
+    @Test
+    void testEqualsSameObject() {
+        assertEquals(_a, _a);
+    }
+
+    @Test
+    void testEqualsNull() {
+        assertNotEquals(_a, null);
+    }
+
+    @Test
+    void testEqualsDifferentClass() { assertFalse(_a.equals(new Coordinate(5))); }
+
+    @Test
+    void testEqualsWhenNotEquals() {
+        assertNotEquals(_a, _b);
+    }
+
+    @Test
+    void testEqualsWhenEquals() { assertEquals(_a, _aSame); }
 }
