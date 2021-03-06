7a22c89e71266a5bd83ecadec8e573f00e9c534d
Meir Klemfner <meirk251@outlook.com>
2021-03-07 00:56:25 +0200

    Implemented the Plane geometry.

---
+++ src/geometries/Plane.java
@@ -0,0 +1,25 @@
+package geometries;
+
+import primitives.Point3D;
+import primitives.Vector;
+
+public class Plane implements Geometry {
+
+    final Point3D _q0;
+    final Vector _normal;
+
+    public Plane(Point3D q0, Vector normal) {
+        _q0 = q0;
+        _normal = normal;
+    }
+
+    public Plane(Point3D a, Point3D b, Point3D c) {
+        _q0 = a;
+        _normal = null; /* TODO: the normal calculation should be implemented next assignment. */
+    }
+
+    @Override
+    public Vector getNormal(Point3D point) {
+        return _normal;
+    }
+}
