d1baf2f564530cf0e8a9e2c33364c0ea6dc57304
Meir Klemfner <meirk251@outlook.com>
2021-03-14 01:03:39 +0200

    Improved unit tests for Point3D.
    Improved the unit tests by following the instructions of exercise #2.

--- unittests/primitives/Point3DTest.java
+++ unittests/primitives/Point3DTest.java
@@ -3,62 +3,85 @@ package primitives;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
+import static primitives.Util.isZero;
 
+/**
+ * Unit tests for {@link primitives.Point3D} class.
+ */
 class Point3DTest {
 
-    Point3D _a = new Point3D(1.1d, 2d, 3d);
-    Point3D _aSame = new Point3D(1.1d, 2d, 3d);
-    Point3D _b = new Point3D(17.123d, -2d, 0.5d);
-
+    /**
+     * Test method for {@link primitives.Point3D#add(primitives.Vector)}.
+     */
     @Test
-    void add() {
-        Point3D addition = _a.add(new Vector(3d, 2.2d, 48d));
-        Point3D result = new Point3D(4.1d, 4.2d, 51d);
-        assertEquals(addition, result);
+    void testAdd() {
+        // ============ Equivalence Partitions Tests ==============
+        Point3D p = new Point3D(1, 2, 3);
+        Vector v = new Vector(-1, -2, -3);
+
+        // test for a proper result
+        assertEquals(Point3D.ZERO, p.add(v), "add() wrong result");
     }
 
+    /**
+     * Test method for {@link primitives.Point3D#subtract(primitives.Point3D)}.
+     */
     @Test
-    void subtract() {
-        Vector subtraction = _a.subtract(_b);
-        Vector result = new Vector(-16.023d, 4d, 2.5d);
-        assertEquals(subtraction, result);
+    void testSubtract() {
+        Point3D p1 = new Point3D(1, 2, 3);
+
+        // ============ Equivalence Partitions Tests ==============
+        Point3D p2 = new Point3D(2, 3, 4);
+        Vector v = new Vector(1, 1, 1);
+
+        // test for a proper result
+        assertEquals(v, p2.subtract(p1), "subtract() wrong result");
+
+        // =============== Boundary Values Tests ==================
+        // test for a zero vector when subtracting with itself
+        try {
+            p1.subtract(p1);
+            fail("subtract() subtraction of itself doesn't throw an exception.");
+        } catch(Exception err) {
+            // expected result: zero vector.
+        }
     }
 
+    /**
+     * Test method for {@link primitives.Point3D#distanceSquared(primitives.Point3D)}.
+     */
     @Test
-    void distanceSquared() {
-        double distanceSquared = _a.distanceSquared(_b);
-        assertEquals(distanceSquared, 278.987, 0.01);
+    void testDistanceSquared() {
+        Point3D p1 = new Point3D(2, 3, 4);
+
+        // ============ Equivalence Partitions Tests ==============
+        Point3D p2 = new Point3D(-2.5, 2, 0);
+
+        // test for a proper result
+        assertTrue(isZero(p1.distanceSquared(p2) - 37.25d), "distanceSquared() wrong result");
+
+        // =============== Boundary Values Tests ==================
+        // test distance to itself is zero
+        assertTrue(isZero(p1.distanceSquared(p1)), "distanceSquared() distance to itself is not zero");
     }
 
+    /**
+     * Test method for {@link primitives.Point3D#distance(primitives.Point3D)}.
+     */
     @Test
-    void distance() {
-        double distance = _a.distance(_b);
-        assertEquals(distance, 16.703, 0.01);
-    }
+    void testDistance() {
+        Point3D p1 = new Point3D(2, 3, 4);
 
-    @Test
-    void testEqualsSameObject() {
-        assertEquals(_a, _a);
-    }
+        // ============ Equivalence Partitions Tests ==============
+        Point3D p2 = new Point3D(2, 3, 0);
 
-    @Test
-    void testEqualsNull() {
-        assertNotEquals(_a, null);
-    }
+        // test for a proper result
+        assertTrue(isZero(p1.distance(p2) - 4d), "distance() wrong result");
 
-    @Test
-    void testEqualsDifferentClass() {
-        assertFalse(_a.equals(new Coordinate(5)));
-    }
-
-    @Test
-    void testEqualsWhenNotEquals() {
-        assertNotEquals(_a, _b);
-    }
-
-    @Test
-    void testEqualsWhenEquals() {
-        assertEquals(_a, _aSame);
+        // =============== Boundary Values Tests ==================
+        // test distance to itself is zero
+        assertTrue(isZero(p1.distance(p1)), "distance() distance to itself is not zero");
     }
 }
