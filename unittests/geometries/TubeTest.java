1dd37c22b95bc165450bd3c96ed0180be9ac5896
Meir Klemfner <meirk251@outlook.com>
2021-03-14 15:25:23 +0200

    Added unit tests for class Tube.

---
+++ unittests/geometries/TubeTest.java
@@ -0,0 +1,60 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for {@link geometries.Tube} class.
+ */
+class TubeTest {
+
+    /**
+     * Test method for {@link geometries.Sphere#Sphere(primitives.Point3D, double)}.
+     */
+    @Test
+    void testConstructor() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test for a proper result.
+        try {
+            new Tube(new Ray(new Point3D(1, -1.5, 0), new Vector(0, 2, 1)), 2);
+        } catch (IllegalArgumentException err) {
+            fail("Failed construct a correct tube");
+        }
+
+        // =============== Boundary Values Tests ==================
+        // TC02: Test when the radius is 0.1
+        try {
+            new Tube(new Ray(new Point3D(1, -1.5, 0), new Vector(0, 2, 1)), 0.1);
+        } catch (IllegalArgumentException err) {
+            fail("Failed construct a correct tube when the radius is 0.1");
+        }
+
+        // TC03: Test when the radius is 0
+        try {
+            new Tube(new Ray(new Point3D(1, -1.5, 0), new Vector(0, 2, 1)), 0);
+            fail("Constructed a tube while the radius is 0");
+        } catch (IllegalArgumentException err) {}
+
+        // TC04: Test when the radius is -0.1
+        try {
+            new Tube(new Ray(new Point3D(1, -1.5, 0), new Vector(0, 2, 1)), -0.1);
+            fail("Constructed a tube while the radius is -0.1");
+        } catch (IllegalArgumentException err) {}
+    }
+
+    /**
+     * Test method for {@link geometries.Tube#getNormal(primitives.Point3D)}.
+     */
+    @Test
+    void testGetNormal() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: There is a simple single test here
+        Tube tb = new Tube(new Ray(new Point3D(1, 1, 0), new Vector(0, 0, 1)), 1d);
+
+        assertEquals(new Vector(0, -1, 0), tb.getNormal(new Point3D(1, 0, 2)), "Bad normal to tube");
+    }
+}
