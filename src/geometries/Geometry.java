ef2773c061a5a6fcf1e40c210978d7224903c1fe
Meir Klemfner <meirk251@outlook.com>
2021-03-20 22:31:52 +0200

    Added comments for the interface Geometry.

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -3,6 +3,14 @@ package geometries;
 import primitives.Vector;
 import primitives.Point3D;
 
+/**
+ * An interface to represent a geometry.
+ */
 public interface Geometry {
+    /**
+     * Returns a normal for a given point on the geometry.
+     * @param point A point on the geometry.
+     * @return A normal vector.
+     */
     public Vector getNormal(Point3D point);
 }
