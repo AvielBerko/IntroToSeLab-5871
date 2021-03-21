437e8b1fcfc816be647e3876e01cc888e7f5a832
Meir Klemfner <meirk251@outlook.com>
2021-03-21 04:15:06 +0200

    Implemented the class Geometries.
    The method findIntersectables is not implemented yet.

---
+++ src/geometries/Geometries.java
@@ -0,0 +1,47 @@
+package geometries;
+
+import primitives.Point3D;
+import primitives.Ray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Geometries class is a collection of intersectables and can calculates their intersections.
+ * It is using Composite design.
+ */
+public class Geometries implements Intersectable {
+    private final List<Intersectable> _intersectables;
+
+    /**
+     * Default constructor.
+     * Creates an empty list of intersectables.
+     */
+    public Geometries() {
+        // ArrayList has constant-time access and the class probably will access the list a lot more
+        // than add to the list, so I chose to use Array List instead of LinkedList.
+        _intersectables = new ArrayList<>();
+    }
+
+    /**
+     * Creates a list of given intersectables.
+     * @param intersectables List of intersectables
+     */
+    public Geometries(Intersectable... intersectables) {
+        _intersectables = new ArrayList<>(Arrays.asList(intersectables));
+    }
+
+    /**
+     * Adds a list of given intersectables to the current list.
+     * @param intersectables List of intersectables to add
+     */
+    public void add(Intersectable... intersectables) {
+        _intersectables.addAll(Arrays.asList(intersectables));
+    }
+
+    @Override
+    public List<Point3D> findIntersections(Ray ray) {
+        return null;
+    }
+}
