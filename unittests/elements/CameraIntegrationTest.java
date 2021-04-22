073c548b0c3f8ef8fd69f94c8c79a0923c6c4668
Meir Klemfner <meirk251@outlook.com>
2021-04-22 17:10:18 +0300

    style: rename CameraIntegrationsTest to CameraIntegrationTest

--- unittests/elements/CameraIntegrationTest.java
+++ unittests/elements/CameraIntegrationTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CameraIntegrationsTest {
+public class CameraIntegrationTest {
 	@Test
 	void testSphereAndCamera() {
 		Sphere[] spheres = new Sphere[]{
